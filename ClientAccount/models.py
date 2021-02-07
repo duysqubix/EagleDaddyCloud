3055ce4faa5823c9785c3319f8e52b373bc72448
duysqubix <duys@qubixat.com>
2021-02-07 03:51:47 +0000

    shhaa dude, forgot the most import app yet, the one that handles accounts.. duh

---
+++ ClientAccount/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from django.contrib.auth import get_user_model
+
+
+class ClientAccount(models.Model):
+    """
+    Represents the main entity of an account
+    """
+    user = models.OneToOneField(get_user_model(),
+                                on_delete=models.CASCADE,
+                                related_name='account')
